6a467f3fec68295daaacdc36b89d8881ce861c63
DenisShilov <kail000@yandex.ru>
2022-09-14 22:28:40 +0300

    Edit slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,46 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
-    variableWidth: true,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+    // responsive: [
+    //   {
+    //     breakpoint: 992,
+    //     settings: {
+    //       dots: true,
+    //       arrows: false
+    //     }}
+    // ]
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });    
 });
