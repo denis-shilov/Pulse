1f77e7a41ebafea46f14acee925770fb58a3092d
DenisShilov <kail000@yandex.ru>
2022-10-20 23:10:43 +0300

    add settings phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -23,13 +23,13 @@ $(document).ready(function(){
 
     function toggleSlide(item) {
         $(item).each(function(i) {
-            $(this).on('click', function(e) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
+            });
         });
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -44,10 +44,10 @@ $(document).ready(function(){
     });
 
     $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
+        $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        })
+        });
     });
 
     function validateForms(form){
@@ -75,12 +75,33 @@ $(document).ready(function(){
                 }
             }
         });
-    };
+    }
 
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation. #order').fadeOut();
+            $('overlay. #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 
