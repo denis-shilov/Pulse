fe4a4c5c9d91029ed44cdd8bbd40bd765de0f7b0
DenisShilov <kail000@yandex.ru>
2022-10-26 21:46:40 +0300

    Add somfing changest with email settings

--- src/js/script.js
+++ src/js/script.js
@@ -23,13 +23,13 @@ $(document).ready(function(){
 
     function toggleSlide(item) {
         $(item).each(function(i) {
-            $(this).on('click', function (e) {
+            $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
+            })
         });
-    }
+    };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -44,10 +44,10 @@ $(document).ready(function(){
     });
 
     $('.button_mini').each(function(i) {
-        $(this).on('click', function () {
+        $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        });
+        })
     });
 
     function validateForms(form){
@@ -75,7 +75,7 @@ $(document).ready(function(){
                 }
             }
         });
-    }
+    };
 
     validateForms('#consultation-form');
     validateForms('#consultation form');
@@ -83,25 +83,38 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    $('form').submit(function (e) {
+    $('form').submit(function(e) {
         e.preventDefault();
-
-        if (!$(this).valid()) {
-            return;
-        }
-
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
             data: $(this).serialize()
-        }).done(function () {
+        }).done(function() {
             $(this).find("input").val("");
-            $('#consultation. #order').fadeOut();
-            $('overlay. #thanks').fadeIn('slow');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
         return false;
     });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
 
